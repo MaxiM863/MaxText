97fee454586c6e2bf490d0061cd6b26b6ecd8d9d
MaxiM863 <97922252+MaxiM863@users.noreply.github.com>
2026-02-13 21:31:32 -0500

    Add a class

--- TextEngine/test.hpp
+++ TextEngine/test.hpp
@@ -4,7 +4,11 @@
 #include <iostream>
 
 namespace MaxText {
+
+    class MaxTest {
+    public:         
     void testFunction() {
         std::cout << "Hello from MaxText!" << std::endl;
     }
+};
 }
